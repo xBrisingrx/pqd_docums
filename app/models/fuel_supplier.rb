cf8150876f9aab923764464e477b52bb1211385a
Mauro Almiron <mdavid.almiron@gmail.com>
2023-04-11 08:57:17 -0300

    change fuel supplier

--- app/models/fuel_supplier.rb
+++ app/models/fuel_supplier.rb
@@ -1,8 +1,26 @@
 class FuelSupplier < ApplicationRecord
-	enum d_type: {
-		people: 1, 
-		vehicles: 2
+	has_many :fuel_loads
+	enum supplier_type: {
+		supplier: 1, 
+		service_station: 2
 	}
 
 	scope :actives, -> { where(active: true) }
+
+	before_create :set_unlimited
+
+	def set_unlimited
+		self.unlimited = self.supplier_type == :service_station
+	end
+
+	def fuel_load
+		cargas = self.fuel_loads.sum(:fueling)
+		descargas = FuelToVehicle.where( fuel_truk_id: self.id ).sum(:fueling)
+
+		cargas - descargas
+	end
+
+	def name_with_fuel
+		"#{self.name} (#{self.fuel_load} lts.)"
+	end
 end
