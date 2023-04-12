54ae2f2a5a55b703d1c8400a52f5cb173f6fbdde
Mauro Almiron <mdavid.almiron@gmail.com>
2023-04-12 01:49:27 -0300

    fix foreigns names

--- db/migrate/20230405160228_create_fuel_to_vehicles.rb
+++ db/migrate/20230405160228_create_fuel_to_vehicles.rb
@@ -3,11 +3,13 @@ class CreateFuelToVehicles < ActiveRecord::Migration[5.2]
     create_table :fuel_to_vehicles do |t|
       t.references :vehicle, foreign_key: true
       t.references :fuel_supplier, foreign_key: true
-      t.references :load, foreign_key: { to_table: 'people' }
-      t.references :authorize, foreign_key: { to_table: 'people' }
+      t.references :person_load, foreign_key: { to_table: 'people' }
+      t.references :person_authorize, foreign_key: { to_table: 'people' }
       t.date :date, null: false
       t.decimal :fueling, null: false
       t.bigint :mileage, null: false
+      t.bigint :ticket, null: false
+      t.integer :fuel_type, default: 1
       t.boolean :active, default: true
 
       t.timestamps
