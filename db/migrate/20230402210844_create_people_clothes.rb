6bb616d57d39a035f7a093790f6332ff43862209
Mauro Almiron <mdavid.almiron@gmail.com>
2023-07-10 22:23:39 -0300

    remove default value

--- db/migrate/20230402210844_create_people_clothes.rb
+++ db/migrate/20230402210844_create_people_clothes.rb
@@ -3,7 +3,7 @@ class CreatePeopleClothes < ActiveRecord::Migration[5.2]
     create_table :people_clothes do |t|
       t.references :person, foreign_key: true
       t.references :clothing_package, foreign_key: true
-      t.text :description, default: ''
+      t.text :description
       t.date :start_date
       t.date :end_date
       t.boolean :active, default: true
